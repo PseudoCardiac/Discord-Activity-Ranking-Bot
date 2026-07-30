fb6e8d113edc476d9d06f23fb588750d13b9f3ab
42 <nooyeahc1125@gmail.com>
2026-07-30 20:45:48 +0900

    afk 채널 이동을 입장/퇴장으로 처리

--- main.py
+++ main.py
@@ -45,30 +45,40 @@ async def on_voice_state_update( member: discord.Member, before: discord.VoiceSt
 
     # on member join
     if ( before.channel is None and after.channel is not None ) and not after.afk and not member.bot:
-        print( f"{ member.display_name } { after.channel.name } 음성 채널에 참가" )
+        # print( f"{ member.display_name } { after.channel.name } 음성 채널에 참가" )
         recordVoiceJoin( str( member.id ) )
 
     # on member leave
     elif ( before.channel is not None and after.channel is None ) and not before.afk and not member.bot:
-        print( f"{ member.display_name } { before.channel.name } 음성 채널에서 나감" )
+        # print( f"{ member.display_name } { before.channel.name } 음성 채널에서 나감" )
         addVoiceTime( str( member.id ) )
 
         if before.self_stream or before.self_video:
             addStreamTime( str( member.id ) )
 
     # on member move
-    # elif before.channel is not None and after.channel is not None and before.channel != after.channel:
+    elif before.channel is not None and after.channel is not None and before.channel != after.channel:
         # print( f"{ member.display_name } { before.channel.name } 음성 채널에서 { after.channel.name } 으로 이동" )
+        # from afk to non-afk
+        if before.afk and not after.afk:
+            recordVoiceJoin( str( member.id ) )
+
+        # from non-afk to afk
+        elif not before.afk and after.afk:
+            addVoiceTime( str( member.id ) )
+
+            if before.self_stream or before.self_video:
+                addStreamTime( str( member.id ) )
 
     # on member live / video start
     if ( not before.self_stream and not before.self_video ) and ( after.self_stream or after.self_video ) and after.channel and not member.bot:
-        print( f"{ member.display_name } { after.channel.name } 음성 채널에서 스트리밍 시작" )
+        # print( f"{ member.display_name } { after.channel.name } 음성 채널에서 스트리밍 시작" )
         recordStreamStart( str( member.id ) )
 
     # on member live / video end
     # WARN: 음성 채널에서 나가면서 스트리밍이 종료된 경우, 이 이벤트가 발생하지 않음
     if ( before.self_stream or before.self_video ) and ( not after.self_stream and not after.self_video ) and before.channel and not member.bot:
-        print( f"{ member.display_name } { before.channel.name } 음성 채널에서 스트리밍 종료" )
+        # print( f"{ member.display_name } { before.channel.name } 음성 채널에서 스트리밍 종료" )
         addStreamTime( str( member.id ) )
 
 
